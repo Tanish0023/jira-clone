d880a897846de112a44b16ab6f0a1f383a1bfca0
Tanish0023 <tanish23aggarwal@gmail.com>
2025-02-21 13:41:46 +0530

    Creating Auth Screens

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import Link from "next/link";
+
+interface SignInLayoutProps {
+    children: React.ReactNode;
+}
+
+const AuthLayout = ({children}: SignInLayoutProps) => {
+    const pathname = usePathname();
+
+    return ( 
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                    <Image src={"/logo.svg"} alt="logo" width={152} height={56} />
+                    <Button asChild variant={"secondary"}>
+                        <Link
+                            href={pathname === "/sign-in" ? "sign-up" : "sign-in"}
+                        >
+                            {pathname === "/sign-in" ? "Sign Up" : "Login"}
+                        </Link>
+                    </Button>
+                </nav>
+                <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+                    {children}
+                </div>
+            </div>
+        </main>
+     );
+}
+ 
+export default AuthLayout;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
+const SignupPage = () => {
+    return ( 
+        <SignUpCard />
+     );
+}
+ 
+export default SignupPage;
